0b79060b6c6eeb8ff92dd69c389dbe23418d9f6b
Shotaro Tsuji <Shotaro.Tsuji@gmail.com>
2021-02-03 00:10:29 +0900

    Make speech body renderer (in progress)

--- src/renderer.rs
+++ src/renderer.rs
@@ -69,6 +69,62 @@ impl HtmlRenderer {
 
         events.push(Event::Html(span_end.into()));
     }
+
+    pub fn render_body<'a>(&self, body: Vec<Inline<'a>>, events: &mut Vec<Event<'a>>) {
+        let mut to_be_trimmed_start = false;
+        let mut event_count = 0usize;
+
+        events.push(Event::Html("<p>".into()));
+
+        for inline in body.into_iter() {
+            match inline {
+                Inline::Event(Event::Text(s)) if to_be_trimmed_start => {
+                    if event_count == 0 {
+                        events.push(Event::Html("<span>".into()));
+                    }
+
+                    let s = s.trim_start().to_owned();
+                    events.push(Event::Text(s.into()));
+                    to_be_trimmed_start = false;
+                    event_count = event_count + 1;
+                },
+                Inline::Event(event) => {
+                    if event_count == 0 {
+                        events.push(Event::Html("<span>".into()));
+                    }
+
+                    events.push(event);
+                    event_count = event_count + 1;
+                },
+                Inline::Direction(direction) => {
+                    trim_end_of_last(events);
+
+                    if event_count > 0 {
+                        events.push(Event::Html("</span>".into()));
+                    }
+
+                    self.render_direction(direction, events);
+                    to_be_trimmed_start = true;
+                    event_count = 0;
+                },
+            }
+        }
+
+        events.push(Event::Html("</p>".into()));
+    }
+}
+
+fn trim_end_of_last<'a>(events: &mut Vec<Event<'a>>) {
+    match events.pop() {
+        Some(Event::Text(s)) => {
+            let s = s.trim_end().to_owned();
+            events.push(Event::Text(s.into()));
+        },
+        Some(event) => {
+            events.push(event);
+        },
+        None => {},
+    }
 }
 
 #[cfg(test)]
@@ -150,6 +206,31 @@ mod test {
         ];
         let mut result = Vec::new();
         HtmlRenderer::default().render_heading(input, &mut result);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn render_body_to_html() {
+        let input = vec![
+            Inline::Event(Event::Text("Hello! ".into())),
+            Inline::Direction(Direction(vec![Event::Text("run".into())])),
+            Inline::Event(Event::Text(" Hello!".into())),
+        ];
+        let expected = vec![
+            Event::Html("<p>".into()),
+            Event::Html("<span>".into()),
+            Event::Text("Hello!".into()),
+            Event::Html("</span>".into()),
+            Event::Html(r#"<span class="direction">"#.into()),
+            Event::Text("run".into()),
+            Event::Html("</span>".into()),
+            Event::Html("<span>".into()),
+            Event::Text("Hello!".into()),
+            Event::Html("</span>".into()),
+            Event::Html("</p>".into()),
+        ];
+        let mut result = Vec::new();
+        HtmlRenderer::default().render_body(input, &mut result);
         assert_eq!(result, expected);
     }
 }
